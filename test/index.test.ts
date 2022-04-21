b21cf70cb8cc76ef16d94331320197316fbf665d
hanlee <me@hanlee.co>
2022-04-21 22:12:11 +0900

    chore: add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,25 @@
-import { describe, expect, it } from 'vitest'
+import { renderHook } from '@testing-library/react-hooks'
+import { describe, expect, it, vi } from 'vitest'
+import { useMount, useUnmount } from '../src'
 
-describe('test', () => {
-  it('exported', () => {
-    expect(1).toBe(1)
+describe('useUnmount', () => {
+  it('only run once on unmounted', async() => {
+    const fn = vi.fn()
+    const hook = renderHook(() => useUnmount(fn))
+    expect(fn).toHaveBeenCalledTimes(0)
+    hook.rerender()
+    expect(fn).toHaveBeenCalledTimes(0)
+    hook.unmount()
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('useMount', () => {
+  it('only run once on mounted', async() => {
+    const fn = vi.fn()
+    const hook = renderHook(() => useMount(fn))
+    expect(fn).toHaveBeenCalledTimes(1)
+    hook.rerender()
+    expect(fn).toHaveBeenCalledTimes(1)
   })
 })
