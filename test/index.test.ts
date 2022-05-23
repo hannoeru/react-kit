9ea4cefd3165e78102ea6f514dfdb041d9de6b8f
hanlee <me@hanlee.co>
2022-05-23 21:24:17 +0900

    chore: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,4 @@
 import { renderHook } from '@testing-library/react-hooks'
-import { describe, expect, it, vi } from 'vitest'
 import { useMount, useUnmount } from '../src'
 
 describe('useUnmount', () => {
