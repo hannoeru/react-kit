a13fc8d67698630a64de4f46311f8b7ba43fd538
hanlee <me@hanlee.co>
2024-02-20 15:23:41 +0900

    fix: immer import

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
-import immer from 'immer'
-import { useSyncExternalStoreWithSelector as useSyncExternalStore } from 'use-sync-external-store/shim/with-selector'
 import type { Draft } from 'immer'
+import { produce } from 'immer'
+import { useSyncExternalStoreWithSelector as useSyncExternalStore } from 'use-sync-external-store/shim/with-selector'
 
 type Listener = Function
 type UpdaterFn<State> = (prevState: State) => State
@@ -40,7 +40,7 @@ export function createStore<State>(initialState: State) {
     },
     mutate(updater) {
       const currState = this.get()
-      const nextState = immer(currState, updater)
+      const nextState = produce(currState, updater)
       if (nextState !== currState) this.set(nextState as State)
     },
   }
